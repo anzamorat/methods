4ab5f4d8d59799025fb753f88d6fb180d16ff4b3
Andrea Zambrano <andrea.zambrano@mail.citytech.cuny.edu>
2019-09-03 15:05:01 -0400

    Exercise 2.1: Write a program that asks for height and how long ball has been falling which return current height of ball.

---
+++ Exercise01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+
+H = 100 #fixed
+g = 9.8
+
+formula = math.sqrt(H*2/g)
+
+print("Time calculated:" , formula)
+
+
+# In[ ]:
+
+
+
+
