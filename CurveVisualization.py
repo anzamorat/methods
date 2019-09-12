9bc40b9c586cc5e78dfe5d50fac22430fad4c759
Andrea Zambrano <andrea.zambrano@mail.citytech.cuny.edu>
2019-09-11 20:34:39 -0400

    CurveVisualization.py

---
+++ CurveVisualization.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # deltoid curve
+
+# ### step 1.
+
+# In[45]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import linspace
+
+th = np.linspace(0,2*np.pi , num =200)
+
+x = 2*np.cos(th)+np.cos(2*th)
+y = 2*np.sin(th)-np.sin(2*th)
+
+print(x)
+print(y)
+
+plt.plot(x,y)
+
+
+# ### step 2.
+
+# In[101]:
+
+
+def f(theta):
+    return th**2
+
+th = np.linspace(0,10*np.pi, num = 200)
+r = f(theta)
+r.size #size
+
+
+# In[102]:
+
+
+x2 = r*np.cos(th)
+y2 = r*np.sin(th)
+
+plt.plot(x2,y2)
+
+
+# ### step 3.
+
+# "Fey's function"
+
+# In[93]:
+
+
+th = np.linspace(0,24*np.pi , num=5000)
+r = (np.exp(np.cos(th)-2*np.cos(4*th)+np.sin(th/12)**5))
+print(r)
+
+
+# In[94]:
+
+
+x3 = r*np.cos(th)
+y3 = r*np.sin(th)
+
+plt.plot(x3,y3)
+
+
+# ## subplots 
+
+# <b> vertically
+
+# In[104]:
+
+
+plt.figure(figsize = (3,6)) 
+plt.subplot(3,1,1)
+plt.plot(x,y, color='m')
+
+
+plt.subplot(3,1,2)
+plt.plot(x2,y2, linestyle = ":")
+
+plt.subplot(3,1,3)
+plt.plot(x3,y3)
+
+
+# <b> horizontally
+
+# In[115]:
+
+
+plt.figure(figsize = (25,6)) 
+plt.subplot(1,3,1)
+plt.plot(x,y, c='y')
+
+
+plt.subplot(1,3,2)
+plt.plot(x2,y2, linestyle=":", c="k")
+
+plt.subplot(1,3,3)
+plt.plot(x3,y3, linestyle="--", c="g")
+
