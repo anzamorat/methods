1e398220dc2691153d247300be4131d82b9264dd
Andrea Zambrano <andrea.zambrano@mail.citytech.cuny.edu>
2019-10-30 23:00:08 -0400

    accuracy/speed laboratory

--- derivatives_integrals.py
+++ derivatives_integrals.py
@@ -4,42 +4,19 @@
 # In[7]:
 
 
-import time 
-import timeit #runs it a million times
-
-
-# In[10]:
-
-
-start=time.time()
-end=time.time()
-print(end-start)
-print(start)
-print(end)
-
-
-# In[11]:
-
-
-#examples
-print(timeit.timeit("5+5/3"))
-print(timeit.timeit("5+5/math.sqrt(3)", "import math"))
-
-
-# In[2]:
-
-
-import math
 import numpy as np
 
 import matplotlib.pyplot as plt
+
+import time 
+import timeit #runs it a million times
 
 
 # <b>Exercise 4.3</b> Derivatives
 #     
 # <b>part A)</b> Write a program that defines a function f(x) returning the value x(x−1), then calculates the derivative of the function at the point x = 1 using the formula above with δ = 10−2
 
-# In[8]:
+# In[13]:
 
 
 #calculating derivatives
@@ -55,12 +32,12 @@ x=1
 
 #derivative
 dfdx=(f(x+delta)-f(x))/(delta)
-print("Derivative %0.2f" % dfdx)
+print("Derivative: %0.4f" % dfdx)
 
 
 # <b> part B)</b> Repeat the calculation for δ = 10−4, 10−6, 10−8, 10−10, 10−12, and 10−14. You should see that the accuracy of the calculation initially gets better as δ gets smaller, but then gets worse again. Plot your result as a function of δ . WHY is this?
 
-# In[60]:
+# In[49]:
 
 
 DELTAS=np.array([1e-4, 1e-6, 1e-8,1e-10,  1e-12 , 1e-14])
@@ -69,29 +46,72 @@ DELTAS=np.array([1e-4, 1e-6, 1e-8,1e-10,  1e-12 , 1e-14])
 dfdx=(f(x+DELTAS)-f(x))/(DELTAS)
 print(dfdx)
 
-plt.plot(np.log10(DELTAS), dfdx, 'gd') #log10 because OG values are tiny
+plt.plot(np.log10(DELTAS), dfdx, 'b>') 
+#log10 because OG values are tiny
 plt.title('Accuracy',fontsize=15)
 plt.xlabel('Delta Values', fontsize=13)
+plt.xlim([-16,-2])
 plt.ylabel('Respective Accuracies', fontsize=13)
 plt.show()
 
 
 # <b>Excercise 4.4</b> Integrals
 
-# In[3]:
+# <b>part a|</b> hint: value of the integral (area under semicircle) must be pi/2
+
+# In[78]:
 
 
 #Riemann definition of the integral
 
+start=time.time()
 def integral(N):
-    top=0
-    h=2/N
-    x=np.linspace(-1,1,100)
-    y=np.sum(np.sqrt(1-x**2))
-    top= h*y
-    return top
+    if N<0:
+        return 0
+    
+    result=0
+    h=2/N #width
+    
+    for k in range(1,N+1):
+        x=-1+h*k
+        y=np.sqrt(1-x**2)
+        result+=h*y #sums over
+        
+    end=time.time() 
+    return result
 
-print(integral(1000))
+print(integral(100))
 
 #N gets more accurate as it increases
+#Hence, 100 slices is not enough!
+
+print(f"Program took :{end-start} seconds to run")
+
+
+# <b>part b
+
+# In[79]:
+
+
+#Increase the value of N (number of slices)
+
+start=time.time()
+def integral(N):
+    if N<0:
+        return 0
+    
+    result=0
+    h=2/N #width
+    
+    for k in range(1,N+1):
+        x=-1+h*k
+        y=np.sqrt(1-x**2)
+        result+=h*y #adds over
+        
+    end=time.time()
+    return result
+        
+print(integral(100000))
+
+print(f"Program took {end-start} seconds to run")
 
