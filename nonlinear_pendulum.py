c42595844bef3d5a58091ea191861533836722bf
Andrea Zambrano <andrea.zambrano@mail.citytech.cuny.edu>
2019-12-03 20:20:21 -0500

    Animation of the movement of a non-linear pendulum

--- nonlinear_pendulum.py
+++ nonlinear_pendulum.py
@@ -2,76 +2,81 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-"NONLINEAR PENDULUM"
+"Non-linear Pendulum"
 
-l=0.1 #u.m
-g=9.81 #c.g0 
-s_theta=179 #degrees
-s_omega=0
+l=0.1 #Length of pendulum arm in meters 
+g=9.81 #Acceleration due to gravity
+s_theta=179 #Starting theta in degrees
+s_omega=0 #Initial omega
 
-#PERIOD=2pi/omega
-#OMEGA=2pi/T
+#Array of initial theta and omega
+r=np.array([s_theta,s_omega]) #r fully depends on theta and omega
 
 def f(r,t):
+    """Return an NxN array consisting of final thetas and final omegas respectively."""
+
     theta=r[0] 
     omega=r[1]
     f_theta=omega
-    f_omega=-(g/l)*np.sin(np.radians(theta))
+    f_omega=-(g/l)*np.sin((theta))
     return np.array([f_theta,f_omega])
 
 "RUNGE-KUTTA"
 
-a=0.0 #start tikme
-b=100.0 #end time
-N=1e4 #total amount of steps
-h=(b-a)/N #single step size of 1e-2
+#Defining variables
+a=0.0 #Start time
+b=100.0 #End time
+N=1e4 #Total amount of steps
+h=(b-a)/N #Single step size of 1e-2
 
-r=np.array([s_theta,s_omega])
+t_points=np.arange(a,b,h) #Range of time points
+theta_points=[] #Empty list to fill for theta_points
 
-t_points=np.arange(a,b,h) 
-theta_points=[]
-
+#For loop
 for t in t_points:
-    theta_points.append(r[0])
+
+    theta_points.append(r[0]) #Appends theta values
     k1 = h*f(r,t)
     k2 = h*f(r+0.5*k1,t+0.5*h)
     k3 = h*f(r+0.5*k2,t+0.5*h)
     k4 = h*f(r+k3,t+h)
 
-    #sums over
-    r = r + (k1+2*k2+2*k3+k4)/6
+    r = r + (k1+2*k2+2*k3+k4)/6 #Sums over
 
-plt.plot(t_points,theta_points, c='r', lw='3')
+#Plot displaying theta points vs time points
+plt.plot(t_points,theta_points, c='m', lw='3')
 plt.title('An epic non-linear pendulum',fontsize=15)
-plt.xlim([0,50])
-plt.show()
 
-"ANIMATION"
+#Zoom in to show detail
+plt.xlim([0,4]) #Limit x-axis
+plt.show() #Displays plot
 
-fig = plt.figure(figsize=(5,5))
-ax = plt.axes(xlim=(-2, 2), ylim=(-2, 2))
+"Animation of Pendulum"
 
-bob = plt.Circle((0,0), radius=0.07, fc='m')
+#Make figure
+fig = plt.figure(figsize=(5,5)) #Size of figure adjustment
+fig.suptitle('An epic non-linear pendulum', fontsize=15, fontweight='bold') #Alterted font style
+ax = plt.axes(xlim=(-2, 2), ylim=(-2, 2)) #Adjusted limits on both axes
 
+#Adds patch
+bob = plt.Circle((0,0), radius=0.07, fc='m') #Circle representing bob of pendulum
+
+#Conditions to begin
 def init():
-    bob.center = (0.1, 0.1)
+    bob.center = (0, 0)
     ax.add_patch(bob)
     return bob,
 
+#Motion of pendulum with derived positions
 def animate(i):
-    x_position = l*np.sin(theta_points[i]) 
-    y_position = l*np.cos(theta_points[i]) 
+    x_position = l*np.cos(theta_points[i]-(np.pi/2)) 
+    y_position = l*np.sin(theta_points[i]-(np.pi/2)) 
     bob.center = (x_position, y_position)
     return bob,
 
-plt.title('An epic non-linear pendulum',fontsize=15)
-plt.xlabel('x', fontsize=13)
-plt.ylabel('y',fontsize=13)
-
+#Executes the animation with initial boundaries
 ani = animation.FuncAnimation(fig, animate,
 init_func=init, frames=360, interval=20, blit=True)
 
-#saves creation
-ani.save('pendulum.gif', writer='pillow')
-
-plt.show()   
+#Saves creation
+ani.save('pretty_pendulum.mp4')
